8a47cc8e71b074a54e38325c7e2271b1e6b53711
Anand <55769839+AK-web@users.noreply.github.com>
2020-11-04 06:01:12 +0530

    initial commit

---
+++ Background_Color_Changer/App.js
@@ -0,0 +1,72 @@
+import React, {useState} from 'react';
+import {
+  Text,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  const [randomColor, setRandomColor] = useState('rgb(32, 0, 126)');
+
+  const changeBG = () => {
+    let color =
+      'rgb(' +
+      Math.floor(Math.random() * 256) +
+      ',' +
+      Math.floor(Math.random() * 256) +
+      ',' +
+      Math.floor(Math.random() * 256) +
+      ')';
+
+    setRandomColor(color);
+  };
+
+  const changeReset = () => {
+    let colorReset = '#000000';
+
+    setRandomColor(colorReset);
+  };
+
+  return (
+    <>
+      <StatusBar backgroundColor={randomColor} />
+      <View style={[styles.container, {backgroundColor: randomColor}]}>
+        <TouchableOpacity onPress={changeBG}>
+          <Text style={styles.text}>Tap Me</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={changeReset}>
+          <Text style={styles.text}>Reset</Text>
+        </TouchableOpacity>
+      </View>
+    </>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+    backgroundColor: '#BB2CD9',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    color: '#FFFFFF',
+    borderRadius: 15,
+    textTransform: 'uppercase',
+  },
+  button: {
+    marginBottom: 20,
+    padding: 30,
+  },
+  space: {
+    width: 20, // or whatever size you need
+    height: 20,
+  },
+});
